6d5ad294dba26a90744e62c4657e49de0faaf556
hadi <hadibakhshi277@gmail.com>
2022-04-18 13:43:49 +0430

    todos completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import "./App.css";
+import AddTodoForm from "./components/Todos/AddTodoForm";
+import TodoList from "./components/Todos/TodoList";
+import TotalCompleteItems from "./components/Todos/TotalCompleteItems";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-     hello
+      <div className="App container">
+        <AddTodoForm />
+        <TodoList />
+        <TotalCompleteItems />
+      </div>
     </div>
   );
 }
-
 export default App;
---
+++ src/features/todos/todosSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [
+    { id: 1, title: "todo1", completed: false },
+    { id: 2, title: "todo2", completed: false },
+    { id: 3, title: "todo3", completed: true },
+    { id: 4, title: "todo4", completed: false },
+    { id: 5, title: "todo5", completed: false },
+  ],
+};
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: Date.now(),
+        title: action.payload.title,
+        completed: false,
+      };
+      state.todos.push(newTodo);
+    },
+
+    toggleTodos: (state, action) => {
+      const selectedTodo = state.todos.find((t) => t.id === action.payload.id);
+      selectedTodo.completed = !selectedTodo.completed;
+    },
+
+    deleteTodos: (state, action) => {
+      const filteredTodos = state.todos.filter(
+        (t) => t.id !== action.payload.id
+      );
+      state.todos = filteredTodos;
+    },
+  },
+});
+
+export const { addTodo, toggleTodos, deleteTodos } = todosSlice.actions;
+export default todosSlice.reducer;
