302aaf8ee993a61a7ae1c04099cfeafce6c16607
hadi <hadibakhshi277@gmail.com>
2022-04-19 12:24:41 +0430

    get todos from backend

--- src/components/Todos/TodoList.jsx
+++ src/components/Todos/TodoList.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TodoItem from "./TodoItem";
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
+import { getAsyncTodos } from "../../features/todos/todosSlice";
 
 const TodoList = () => {
 
-const {todos} = useSelector(state => state.todos);
+const {todos,error, loading} = useSelector(state => state.todos);
+const dispatch = useDispatch();
+
+
+useEffect(()=> {
+  dispatch(getAsyncTodos())
+},[])
+
+if(loading) return <p>Loading...</p>
+
+if(error) return <p>{error}</p>
 
   return (
     <ul className="list-group">
       {todos.map((todo) => (
-        <TodoItem key={todo.id} id={todo.id} title={todo.title} completed={todo.completed} />
+        <TodoItem key={todo.id} id={todo.id} {...todo} />
       ))}
     </ul>
   );
